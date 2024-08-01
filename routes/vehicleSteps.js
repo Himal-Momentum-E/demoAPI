52843612ef0793f8bc5139f960c5af86c5f08874
Himal Gautam <himalpgautam@gmail.com>
2024-08-01 11:49:21 +0530

    added step 2

--- routes/vehicleSteps.js
+++ routes/vehicleSteps.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Step1 = require("../models/step1");
+const Step2 = require("../models/step2");
 const Step3 = require("../models/step3");
+// const Step4 = require("../models/step4");
+// const Step7 = require("../models/step7");
 
 router.get("/", async (req, res) => {
   const vehicleId = req.query.vehicleId; // Use req.query for query parameters
@@ -15,13 +18,29 @@ router.get("/", async (req, res) => {
     }
 
     let data;
-    if (stepId === "1") {
-      data = await Step1.find({ vehicleId });
-    } else if (stepId === "3") {
-      data = await Step3.find({ vehicleId });
-    } else {
-      return res.status(400).json({ error: "Invalid step ID" });
+    let Model;
+
+    switch (stepID) {
+      case "1":
+        Model = Step1;
+        break;
+      case "2":
+        Model = Step2;
+        break;
+      case "3":
+        Model = Step3;
+        break;
+      // case "4":
+      //   Model = Step4;
+      //   break;
+      // case "7":
+      //   Model = Step7;
+      //   break;
+      default:
+        return res.status(400).json({ error: "Invalid step ID" });
     }
+
+    data = await Model.find({ vehicleID }); // Use the dynamically selected model
 
     if (!data.length) {
       return res
